49ed05c44c75a7e79bb518f702ebc7fdae5c0361
Nick Xie <nick@khadas.com>
2026-04-29 11:17:19 +0800

    Revert "ispHal: ov08a10: disable debug information"
    This reverts commit 884150c3417e7d87c1a39801832fd1f41be638c5.

--- linux_54_t7c/ispHal/sensor_calibration/ov08a10/ov08a10_sdr_calibration.c
+++ linux_54_t7c/ispHal/sensor_calibration/ov08a10/ov08a10_sdr_calibration.c
@@ -164,7 +164,7 @@ static int32_t _CALIBRATION_AE_CTL[32] = {
     16,       // max exposure ratio limit, this is times.
     (200*(1<<10)),  //  Light intensity at full exposure and zero gain , exp: 128lux = 128*(1<<10)
     2,       //feedback delay frame numbers of stats info in current system
-    0,       //ae debug:bit[0] target, [1] ratio, [2] exposure calculate
+    2,       //ae debug:bit[0] target, [1] ratio, [2] exposure calculate
 };
 
 static uint32_t _CALIBRATION_AE_ROI[16][7] = {
